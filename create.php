dc407985ac4f0a1df8093bc478dda1e76d24e8c5
Jenna <80350744+jaebrn@users.noreply.github.com>
2023-11-07 18:32:57 -0500

    Blendable image selection
    Small set of images to blend;
    when selected, the image is highlighted. Selected image urls are added to an array. Toggles when re-selected.

--- create.php
+++ create.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<style>
+    .highlight {
+        border: 2px solid red;
+    }
+</style>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,9 +25,70 @@
     require('constants/sidebar.php');
     ?>
 
+    <button id="button"> Send request </button>
+    <br>
+    <img src="blendables/Adapter.jpg" height="250px" width="auto" class="blendable">
+    <img src="blendables/Ev-1.jpg" height="250px" width="auto" class="blendable">
+    <img src="blendables/pinch.png" height="250px" width="auto" class="blendable">
+    <img src="blendables/wires.jpg" height="250px" width="auto" class="blendable">
+
+
+
     <?php
     require('constants/footer.php');
     ?>
 </body>
+
+
+<script>
+    window.onload = function() {
+        document
+            .querySelector("#button")
+            .addEventListener("click", function() {
+                console.log("button press");
+                makeFetchRequest();
+            })
+
+        clearSelection();
+    };
+
+    const makeFetchRequest = async () => {
+
+    }
+
+    //URLs of highlighted images
+    let highlightedImages = [];
+
+
+    function handleImageClick(event) {
+        const clickedImage = event.target;
+
+        // Toggle highlighting and add/remove URL from the array
+        if (clickedImage.classList.contains('highlight')) {
+            clickedImage.classList.remove('highlight');
+
+            const url = clickedImage.src;
+            const index = highlightedImages.indexOf(url);
+            if (index > -1) {
+                highlightedImages.splice(index, 1);
+            }
+        } else {
+            clickedImage.classList.add('highlight');
+            highlightedImages.push(clickedImage.src);
+        }
+        console.log('Highlighted Images:', highlightedImages);
+    }
+
+    // Add click event listener to all images with class 'blendable'
+    const images = document.querySelectorAll('.blendable');
+    images.forEach(image => {
+        image.addEventListener('click', handleImageClick);
+    });
+
+    function clearSelection() {
+        highlightedImages = [];
+        console.log('Array cleared:', highlightedImages);
+    }
+</script>
 
 </html>
