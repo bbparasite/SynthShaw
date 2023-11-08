3fbfd277646bc9c73aa3ac3e0a93c3a9988d9aa5
Jenna <80350744+jaebrn@users.noreply.github.com>
2023-11-07 20:42:03 -0500

    Blended gallery content

--- gallery.php
+++ gallery.php
@@ -15,79 +15,80 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <!-- Above credits for Album example on Bootstrap (getbootstrap.com)-->
     <style>
-      .gallery-item {
-          cursor: pointer;
-          overflow: hidden;
-          border-radius: 4px;
-      }
+        .gallery-item {
+            cursor: pointer;
+            overflow: hidden;
+            border-radius: 4px;
+        }
 
-      .gallery-item img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-          transition: 0.3s ease-in-out;
-      }
+        .gallery-item img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            transition: 0.3s ease-in-out;
+        }
 
-      .gallery-item img:hover {
-          transform: scale(1.1);
-      }
+        .gallery-item img:hover {
+            transform: scale(1.1);
+        }
 
-      /*Image modal*/
+        /*Image modal*/
 
-      .modal {
-          width: 100%;
-          height: 100%;
-          position: fixed;
-          top: 0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: rgba(0, 0, 0, 0.733);
-          margin-top: -1px;
-          animation: zoom 0.3s ease-in-out;
-      }
+        .modal {
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            top: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgba(0, 0, 0, 0.733);
+            margin-top: -1px;
+            animation: zoom 0.3s ease-in-out;
+        }
 
-      .modal img {
-          width: 50%;
-          object-fit: cover;
-      }
+        .modal img {
+            width: 50%;
+            object-fit: cover;
+        }
 
-      /*Close modal*/
-      .closeBtn {
-          color: rgba(255, 255, 255, 0.87);
-          font-size: 25px;
-          position: absolute;
-          top: 0;
-          right: 0;
-          margin: 20px;
-          cursor: pointer;
-          transition: 0.2s ease-in-out;
-      }
+        /*Close modal*/
+        .closeBtn {
+            color: rgba(255, 255, 255, 0.87);
+            font-size: 25px;
+            position: absolute;
+            top: 0;
+            right: 0;
+            margin: 20px;
+            cursor: pointer;
+            transition: 0.2s ease-in-out;
+        }
 
-      .closeBtn:hover {
-          color: rgb(255, 255, 255);
-      }
+        .closeBtn:hover {
+            color: rgb(255, 255, 255);
+        }
 
-      @keyframes zoom {
-          from {
-              transform: scale(0);
-          }
-          to {
-              transform: scale(1);
-          }
-      }
+        @keyframes zoom {
+            from {
+                transform: scale(0);
+            }
 
-      @media (max-width: 950px) {
-          .album {
-              grid-template-columns: repeat(2, 1fr);
-          }
-      }
+            to {
+                transform: scale(1);
+            }
+        }
 
-      @media (max-width: 550px) {
-          .album {
-              grid-template-columns: repeat(1, 1fr);
-          }
-      }
+        @media (max-width: 950px) {
+            .album {
+                grid-template-columns: repeat(2, 1fr);
+            }
+        }
+
+        @media (max-width: 550px) {
+            .album {
+                grid-template-columns: repeat(1, 1fr);
+            }
+        }
     </style>
 </head>
 
@@ -97,19 +98,52 @@
     require('constants/sidebar.php');
     ?>
     <div class="album py-5 bg-white">
-    <div class="container">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <div class="col gallery-item">
-          <div class="card shadow-sm">
-            <img class="card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" src="images/synthshaw.PNG"><title>Placeholder</title><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></img>
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col gallery-item">
+                    <div class="card shadow-sm">
+                        <img class="card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" src="images/blended/daptshaw.jpg">
+                        <title>Placeholder</title><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></img>
+                        <div class="card-body">
+                            <p class="card-text">Blend by Evelyn Scrimshaw, 2023.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+
+    <div class="album py-5 bg-white">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col gallery-item">
+                    <div class="card shadow-sm">
+                        <img class="card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" src="images/blended/reDrawn.jpg">
+                        <title>Placeholder</title><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></img>
+                        <div class="card-body">
+                            <p class="card-text">Blend by Evelyn Scrimshaw, retouched by hand, 2023.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="album py-5 bg-white">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col gallery-item">
+                    <div class="card shadow-sm">
+                        <img class="card-img-top" width="100%" height="225" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" src="images/blended/smoothie.jpg">
+                        <title>Placeholder</title><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></img>
+                        <div class="card-body">
+                            <p class="card-text">Blend by Evelyn Scrimshaw, 2023.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php
     require('constants/footer.php');
     ?>
